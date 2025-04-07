d72c8081c92785e9deb08f10d2f04a0790aaf8e3
Max <clashshow2000@gmail.com>
2025-04-07 20:58:09 +0500

    first commit

---
+++ src/main/java/com/kiramon87/urfudebtproject/UrfuDebtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kiramon87.urfudebtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrfuDebtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrfuDebtProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiramon87/urfudebtproject/UrfuDebtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiramon87.urfudebtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuDebtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
